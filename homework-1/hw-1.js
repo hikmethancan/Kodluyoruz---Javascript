5be0805d4715b13e0192a34105d359eff5bb4d14
hikmethancan <hikmethann@gmail.com>
2021-11-08 14:07:29 +0300

    clock

--- homework-1/hw-1.js
+++ homework-1/hw-1.js
@@ -3,9 +3,15 @@ let myName = document.querySelector("#myName");
 
 myName.innerHTML = `${info.toUpperCase()} `;
 
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-document.querySelector("#myClock").innerHTML = ` ${time}  ${date} ` ;
+function digitalSaat() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    document.querySelector("#myClock").innerHTML = ` ${time}  ${date} ` ;
+    
+}
+
+var time = setInterval(digitalSaat,100);
